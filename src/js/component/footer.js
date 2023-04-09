0caaee29e0962a250753c684a7a93fca8f2e417b
TrinidadMartiG <trinidad.martig@gmail.com>
2023-04-09 19:50:54 +0000

    changed link color

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="https://github.com/TrinidadMartiG">Trini</a>
-		</p>
+		<span class="badge bg-dark">
+				Made with <i className="fa fa-heart text-danger" /> by <span></span>
+				<a href="https://github.com/TrinidadMartiG" className="link-warning">Trini</a>
+		</span>
 	</footer>
 );
